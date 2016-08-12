7f50ad3ff8f9d771a2570504224c2e1e8c0b4052
oliegjio <oliegjio@gmail.com>
2016-08-12 21:23:03 +0300

    Reimplemented progress bars from skills section

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -6,72 +6,43 @@
 ///<reference path="d.ts/progressbar.d.ts" />
 
 $(() => {
-  let $welcome_section_shadow: JQuery = $('.welcome-section__shadow').first();
+  let $welcomeSectionShadow: JQuery = $('.welcome-section__shadow').first();
   let $header: JQuery = $('.header').first();
   let $carousel: JQuery = $('.header-carousel').first();
   let $body: JQuery = $('body');
-  let $open_offcanvas: JQuery = $('#open-offcanvas');
-  let $site_overlay: JQuery = $('.site-overlay').first();
-  let $offcanvas_menu: JQuery = $('.offcanvas-menu').first();
-  let $parallax_break: JQuery = $('.parallax-break');
+  let $openOffcanvas: JQuery = $('#open-offcanvas');
+  let $siteOverlay: JQuery = $('.site-overlay').first();
+  let $offcanvasMenu: JQuery = $('.offcanvas-menu').first();
+  let $parallaxBreak: JQuery = $('.parallax-break');
 
   if($body.scrollTop() > 0) {
     $header.addClass('header--fixed');
   }
 
-  let $progress_bars: JQuery = $('.skills-section__progress-bar');
-  let progress_bars_instances: Array<any> = [];
-  let $progress_bars_labels: JQuery = $('.skills-section__progress-percent');
-  let progress_bars_percentages: Array<number> = [];
-  let progress_bars_coordinates: Array<number> = [];
+  let $progressBars: JQuery = $('.skills-section__progress-bar');
+  let $progressBarsLabels: JQuery = $('.skills-section__progress-percent');
+  let progressBarsPercentages: Array<number> = [];
 
-  for(let i = 0; i < $progress_bars.length; i++) {
-    if($progress_bars_labels[i].innerHTML.length === 3) {
-      progress_bars_percentages[i] = parseInt($progress_bars_labels[i].innerHTML.substr(0, 2));
-    } else if ($progress_bars_labels[i].innerHTML.length === 2) {
-      progress_bars_percentages[i] = parseInt($progress_bars_labels[i].innerHTML.substr(0, 1));
-    }
+  for(let i = 0; i < $progressBars.length; i++) {
+    progressBarsPercentages[i] = parseInt($progressBarsLabels[i].innerHTML.replace('%', ''));
+    let progress: number = 30;
 
-    progress_bars_instances[i] = new ProgressBar.Line($progress_bars[i], {
-      easing: 'easeOut',
-      strokeWidth: 3,
-      color: '#0099CC',
-      svgStyle: {
-        'border-radius': '10px',
-        'height': '20px',
-        'width': '100%'
-      },
-      duration: 3000
-    });
+    let interval: number = setInterval(() => {
+      let progressBar = $($progressBars[i]);
+      let label = $($progressBarsLabels[i]);
 
-    let path = progress_bars_instances[i].path;
-    path.style.strokeLinecap = 'round';
-    progress_bars_instances[i].set(0.5);
-    progress_bars_instances[i].setText($progress_bars_labels[i]);
+      progressBar.css('width', progress + '%');
 
-    progress_bars_instances[i].animate(progress_bars_percentages[i] / 100);
+      progress += 0.5;
+      console.log(progress);
 
-    setTimeout(() => {
-      let progress_bar_width: number = $($progress_bars[0]).outerWidth(true);
-      let percent = (progress_bar_width / 100) * progress_bars_percentages[i];
-      let label = $($progress_bars_labels[i]);
-      label.css('left', (progress_bar_width / 2) - (progress_bar_width - percent) - label.width() + 'px');
-      setTimeout(() => {
-        label.css('transition', 'none');
-      }, 2500);
-    }, 250)
+      if(progress === progressBarsPercentages[i]) {
+        clearInterval(interval);
+      }
+    }, 10);
   }
 
-  $(window).resize(() => {
-    for(let i = 0; i < $progress_bars_labels.length; i++) {
-      let progress_bar_width: number = $($progress_bars[0]).outerWidth(true);
-      let percent = (progress_bar_width / 100) * progress_bars_percentages[i];
-      let label = $($progress_bars_labels[i]);
-      label.css('left', (progress_bar_width / 2) - (progress_bar_width - percent) - label.width() + 'px');
-    }
-  });
-
-  $parallax_break.parallax({imageSrc: '../images/future/sky2_1.jpg'});
+  $parallaxBreak.parallax({imageSrc: '../images/future/sky2_1.jpg'});
 
   $header.scrollspy({
     min: 1,
@@ -88,7 +59,7 @@ $(() => {
 
   $carousel.slick({
     // autoplay: true,
-    appendArrows: $welcome_section_shadow,
+    appendArrows: $welcomeSectionShadow,
     autoplaySpeed: 2000,
     arrows: true,
     fade: true,
@@ -97,12 +68,12 @@ $(() => {
     swipe: false
   });
 
-  $open_offcanvas.click(() => {
-    $open_offcanvas.toggleClass('hamburger--open');
+  $openOffcanvas.click(() => {
+    $openOffcanvas.toggleClass('hamburger--open');
   });
 
-  $site_overlay.click(() => {
-    $open_offcanvas.removeClass('hamburger--open');
+  $siteOverlay.click(() => {
+    $openOffcanvas.removeClass('hamburger--open');
   });
 
 });
--- js/main.js
+++ js/main.js
@@ -1,65 +1,36 @@
 $(function () {
-    var $welcome_section_shadow = $('.welcome-section__shadow').first();
+    var $welcomeSectionShadow = $('.welcome-section__shadow').first();
     var $header = $('.header').first();
     var $carousel = $('.header-carousel').first();
     var $body = $('body');
-    var $open_offcanvas = $('#open-offcanvas');
-    var $site_overlay = $('.site-overlay').first();
-    var $offcanvas_menu = $('.offcanvas-menu').first();
-    var $parallax_break = $('.parallax-break');
+    var $openOffcanvas = $('#open-offcanvas');
+    var $siteOverlay = $('.site-overlay').first();
+    var $offcanvasMenu = $('.offcanvas-menu').first();
+    var $parallaxBreak = $('.parallax-break');
     if ($body.scrollTop() > 0) {
         $header.addClass('header--fixed');
     }
-    var $progress_bars = $('.skills-section__progress-bar');
-    var progress_bars_instances = [];
-    var $progress_bars_labels = $('.skills-section__progress-percent');
-    var progress_bars_percentages = [];
-    var progress_bars_coordinates = [];
+    var $progressBars = $('.skills-section__progress-bar');
+    var $progressBarsLabels = $('.skills-section__progress-percent');
+    var progressBarsPercentages = [];
     var _loop_1 = function(i) {
-        if ($progress_bars_labels[i].innerHTML.length === 3) {
-            progress_bars_percentages[i] = parseInt($progress_bars_labels[i].innerHTML.substr(0, 2));
-        }
-        else if ($progress_bars_labels[i].innerHTML.length === 2) {
-            progress_bars_percentages[i] = parseInt($progress_bars_labels[i].innerHTML.substr(0, 1));
-        }
-        progress_bars_instances[i] = new ProgressBar.Line($progress_bars[i], {
-            easing: 'easeOut',
-            strokeWidth: 3,
-            color: '#0099CC',
-            svgStyle: {
-                'border-radius': '10px',
-                'height': '20px',
-                'width': '100%'
-            },
-            duration: 3000
-        });
-        var path = progress_bars_instances[i].path;
-        path.style.strokeLinecap = 'round';
-        progress_bars_instances[i].set(0.5);
-        progress_bars_instances[i].setText($progress_bars_labels[i]);
-        progress_bars_instances[i].animate(progress_bars_percentages[i] / 100);
-        setTimeout(function () {
-            var progress_bar_width = $($progress_bars[0]).outerWidth(true);
-            var percent = (progress_bar_width / 100) * progress_bars_percentages[i];
-            var label = $($progress_bars_labels[i]);
-            label.css('left', (progress_bar_width / 2) - (progress_bar_width - percent) - label.width() + 'px');
-            setTimeout(function () {
-                label.css('transition', 'none');
-            }, 2500);
-        }, 250);
+        progressBarsPercentages[i] = parseInt($progressBarsLabels[i].innerHTML.replace('%', ''));
+        var progress = 30;
+        var interval = setInterval(function () {
+            var progressBar = $($progressBars[i]);
+            var label = $($progressBarsLabels[i]);
+            progressBar.css('width', progress + '%');
+            progress += 0.5;
+            console.log(progress);
+            if (progress === progressBarsPercentages[i]) {
+                clearInterval(interval);
+            }
+        }, 10);
     };
-    for (var i = 0; i < $progress_bars.length; i++) {
+    for (var i = 0; i < $progressBars.length; i++) {
         _loop_1(i);
     }
-    $(window).resize(function () {
-        for (var i = 0; i < $progress_bars_labels.length; i++) {
-            var progress_bar_width = $($progress_bars[0]).outerWidth(true);
-            var percent = (progress_bar_width / 100) * progress_bars_percentages[i];
-            var label = $($progress_bars_labels[i]);
-            label.css('left', (progress_bar_width / 2) - (progress_bar_width - percent) - label.width() + 'px');
-        }
-    });
-    $parallax_break.parallax({ imageSrc: '../images/future/sky2_1.jpg' });
+    $parallaxBreak.parallax({ imageSrc: '../images/future/sky2_1.jpg' });
     $header.scrollspy({
         min: 1,
         max: $body.height(),
@@ -73,7 +44,7 @@ $(function () {
         }
     });
     $carousel.slick({
-        appendArrows: $welcome_section_shadow,
+        appendArrows: $welcomeSectionShadow,
         autoplaySpeed: 2000,
         arrows: true,
         fade: true,
@@ -81,11 +52,11 @@ $(function () {
         speed: 2500,
         swipe: false
     });
-    $open_offcanvas.click(function () {
-        $open_offcanvas.toggleClass('hamburger--open');
+    $openOffcanvas.click(function () {
+        $openOffcanvas.toggleClass('hamburger--open');
     });
-    $site_overlay.click(function () {
-        $open_offcanvas.removeClass('hamburger--open');
+    $siteOverlay.click(function () {
+        $openOffcanvas.removeClass('hamburger--open');
     });
 });
 
