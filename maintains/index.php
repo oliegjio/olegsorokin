92866ddf7d3ce31e55d3b05c24a04d09604283fd
oliegjio <oliegjio@gmail.com>
2016-08-20 18:21:59 +0300

    Maintains page improve

--- maintains/index.php
+++ maintains/index.php
@@ -1,18 +1,31 @@
 <!doctype html>
-<title>Site Maintenance</title>
-<style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
-</style>
+<head>
+  <title>Site Maintenance</title>
 
-<article>
-    <h1>I&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience but I am performing some maintenance at the moment. If you need to you can always <a href="https://vk.com/id225106539">contact Me</a>, otherwise I&rsquo;ll be back online shortly!</p>
-        <p>&mdash; Oleg Sorokin</p>
-    </div>
-</article>
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
+  <link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32"/>
+  <link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16"/>
+  <link rel="manifest" href="favicon/manifest.json"/>
+  <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+  <link rel="shortcut icon" href="favicon/favicon.ico"/>
+  <meta name="msapplication-config" content="favicon/browserconfig.xml"/>
+  <meta name="theme-color" content="#ffffff"/>
+
+  <style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #0099CC; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+  </style>
+</head>
+<body>
+  <article>
+      <h1>I&rsquo;ll be back soon!</h1>
+      <div>
+          <p>Sorry for the inconvenience but I am performing some maintenance at the moment. If you need to you can always <a href="https://vk.com/id225106539">contact Me</a>, otherwise I&rsquo;ll be back online shortly!</p>
+          <p>&mdash; Oleg Sorokin</p>
+      </div>
+  </article>
+</body>
