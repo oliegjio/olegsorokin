12b1609e4f1b08ccbdb566dd5c1f08ea84bb93a3
oliegjio <oliegjio@gmail.com>
2016-10-28 18:35:53 +0300

    Making E-Mail contact support

--- public/mail.php
+++ public/mail.php
@@ -2,11 +2,9 @@
 
 $to = 'oliegjio@gmail.com';
 $subject = 'OLEGSOROKIN.COM';
-$message = 'Message: ' . $_POST['message'] .
-           '<br>From: ' . $_POST['name'] .
-           '<br>With E-Mail: ' . $_POST['email'];
-// $headers = 'From: oliegjio@gmail.com' . '\r\n' .
-           // 'Reply-To: NOREPLY' . '\r\n' .
-           // 'X-Mailer: PHP/' . phpversion();
+$message = 
+    '(From: ' . $_POST['name'] . ')   ' .
+    '(With E-Mail: ' . $_POST['email'] . ')   ' .
+    'Message: ' . $_POST['message'] .
 
 mail($to, $subject, $message);
